ab34fba2c1403546b3548511cecc8e0bf1562265
Christian Todd <christiantodd27@icloud.com>
2017-04-01 17:44:12 -0400

    adjust stying and add scroll function.

--- app.js
+++ app.js
@@ -9,5 +9,14 @@ function getUserLocation (){
     e.preventDefault();
     var location = $('#location-form input').val();
     this.reset();
+    $('html, body').animate({
+      scrollTop: $('.wrapper').offset().top
+    }, 1000);
+    renderUserLocation(location);
   });
+  
+}
+
+function renderUserLocation (location){
+  $('#location').text(location);
 }
